1221ea64db394b993077306525388fb860958285
Piotr Skrobek <p.skrobek@45stages.com>
2022-10-26 15:12:23 +0200

    add debugger in VSC, tested

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -3,5 +3,9 @@
 int main()
 {
     std::cout<< "Hello in Tutorial Project2 for Visual Studio Code"<<std::endl;
+    int value {100};
+    std::cout<< "type some value "<<std::endl;
+    value += 10;
+    std::cout<<"Your value + 10 is"<< value;
     return 0;
 }
