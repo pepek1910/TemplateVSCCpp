805050b2acfe7de4f888519f4ceac3d82da3bf6b
Piotr Skrobek <p.skrobek@45stages.com>
2022-10-26 14:37:06 +0200

    VSC Configured for mingw64 for build and run multiple project in folder

---
+++ Project1/mian.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<< "Hello in Tutorial Project1 for Visual Studio Code"<<std::endl;
+    return 0;
+}
---
+++ Project2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<< "Hello in Tutorial Project2 for Visual Studio Code"<<std::endl;
+    return 0;
+}
